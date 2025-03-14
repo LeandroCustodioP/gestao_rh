9ce9dc2474c868aa9e8432ece1574f0bf7be331a
LeandroCustodioP <leancp@gmail.com>
2025-03-14 19:00:16 -0300

    Criando os relacionamentos de banco de dados

--- funcionarios/models.py
+++ funcionarios/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from departamentos.models import Departamento
+from empresas.models import Empresa
 
 
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100, help_text='Nome do Funcionário')
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    departamento = models.ManyToManyField(Departamento)
+    empresa = models.ForeignKey(Empresa, on_delete=models.SET_NULL, null=True, blank=True) 
 
     def __str__(self):
         return self.nome
--- registro_hora_extra/models.py
+++ registro_hora_extra/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from funcionarios.models import Funcionario
+
 
 # Create your models here.
 class RegistroHoraExtra(models.Model):
     motivo = models.CharField(max_length=100, help_text='Motimo da Hora Extra')
+    funcionario = models.ForeignKey(Funcionario, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.motivo
